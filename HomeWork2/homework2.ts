a91e930897610ce7e15d0c9e0e3a94c2b069b937
Jeredas <efioss93@mail.ru>
2021-10-03 00:11:56 +0300

    feat: add 3rd task for homework2

---
+++ HomeWork2/homework2.ts
@@ -0,0 +1,21 @@
+// Task 3
+
+const myIterable = { 
+    from: 1,
+    to: 4,
+    [Symbol.iterator]: function *() {
+        let from = parseInt(this.from);
+        let to =parseInt(this.to);
+        if(isNaN(from) || isNaN(to)) throw new TypeError('Only numbers allowed');
+        if(from > to) throw new RangeError('"From" can\'t be bigger than "to"');
+        for(let i = from; i <= to; i++){
+            yield i;
+        };
+    },
+};
+
+for (let item of myIterable) {
+    console.log(item); // 1, 2, 3, 4
+   }
+
+   
