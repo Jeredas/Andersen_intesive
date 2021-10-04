39b552650ff89cd6d66c99c49a65db4b1abe7a75
Jeredas <efioss93@mail.ru>
2021-10-04 11:56:04 +0300

    feat: add task 1 & task 2

--- HomeWork2/homework2.ts
+++ HomeWork2/homework2.ts
@@ -1,3 +1,31 @@
+const makeObjectDeepCopy = (targetObject) => {
+    if (typeof targetObject !== 'object' || targetObject === null) {
+      return targetObject 
+    };
+    let result = Array.isArray(targetObject) ? [] : {};
+    for (let key in targetObject) {
+      let value = targetObject[key];
+      result[key] = makeObjectDeepCopy(value);
+    }
+    return result;
+  }
+
+const selectFromInterval = (arr: number[], first: number, second: number) => {
+   if(
+    arr.some((elem)=> elem !== +elem) ||
+    first !== +first ||
+    second !== +second
+     ) throw new TypeError('Array items should be numbers');
+    const interval = arr.filter((elem) => {
+        if(first > second) {
+            return elem <= first && elem >= second; 
+        } else {
+            return elem >= first && elem <= second;
+        }; 
+    });
+    return interval.sort();
+}
+
 const myIterable = { 
     from: 1,
     to: 4,
