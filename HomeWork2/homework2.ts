91a5a10c7e011a2e4ead7b4ced176d78735b5f3f
Jeredas <efioss93@mail.ru>
2021-10-04 13:21:07 +0200

    feat: add check for integer

--- HomeWork2/homework2.ts
+++ HomeWork2/homework2.ts
@@ -31,11 +31,12 @@ const myIterable = {
     from: 1,
     to: 4,
     [Symbol.iterator]: function *() {
-        let from = parseInt(this.from);
-        let to =parseInt(this.to);
-        if(isNaN(from) || isNaN(to)) throw new TypeError('Only numbers allowed');
-        if(from > to) throw new RangeError('"From" can\'t be bigger than "to"');
-        for(let i = from; i <= to; i++){
+        if(
+            isNaN(this.from) || !Number.isInteger(this.from) ||
+            isNaN(this.to) || !Number.isInteger(this.to)
+        ) throw new TypeError('Only integers allowed');
+        if(this.from > this.to) throw new RangeError('"From" can\'t be bigger than "to"');
+        for(let i = this.from; i <= this.to; i++){
             yield i;
         };
     },
