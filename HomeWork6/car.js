f9087355708a5589751761aff3198e24b09a6697
Jeredas <efioss93@mail.ru>
2021-10-19 00:14:26 +0300

    fix: remove logs

--- HomeWork6/car.js
+++ HomeWork6/car.js
@@ -1,10 +1,10 @@
  class Car {
     #brand;
     #model;
-    #yearOfManufacturing
-    #maxSpeed
-    #maxFuelVolume
-    #fuelConsumption
+    #yearOfManufacturing;
+    #maxSpeed;
+    #maxFuelVolume;
+    #fuelConsumption;
     #currentFuelVolume = 0;
     #isStarted = false;
     #mileage = 0;
@@ -12,7 +12,7 @@
     constructor() {
     }
     set brand(brandName) {
-        if (brandName === `${brandName}`) {
+        if (brandName === `${brandName}` || brandName.length > 1 || brandName < 50) {
             this.#brand = brandName
         } else {
             throw new TypeError('Brand name should be a string')
@@ -22,7 +22,7 @@
         return this.#brand;
     };
     set model(modelName) {
-        if (modelName === `${modelName}`) {
+        if (modelName === `${modelName}`|| modelName.length > 1 || modelName < 50) {
             this.#model = modelName;
         } else {
             throw new TypeError('Model name should be a string')
@@ -42,7 +42,7 @@
         return this.#yearOfManufacturing;
     };
     set maxSpeed(speed) {
-        if (!Number.isSafeInteger(speed)) throw new TypeError('Speed should be safe integer');
+        if (!Number.isFinite(speed)) throw new TypeError('Speed should be a finite integer');
         if (speed < 100 || speed > 300) {
             throw new RangeError('speed can\'t be less than 100 and bigger than 300');
         };
@@ -52,7 +52,7 @@
         return this.#maxSpeed;
     };
     set maxFuelVolume(fuelVolume) {
-        if (!Number.isSafeInteger(fuelVolume)) throw new TypeError('Fuel volume should be safe integer');
+        if (!Number.isFinite(fuelVolume)) throw new TypeError('Fuel volume should be a finite integer');
         if (fuelVolume < 5 || fuelVolume > 20) {
             throw new RangeError('Fuel volume can\'t be less than 5 and bigger than 20');
         };
@@ -64,12 +64,12 @@
     get currentFuelVolume() {
         return this.#currentFuelVolume;
     };
-    set fuelConsumption(fuelConsumption) {
-        if (!Number.isSafeInteger(fuelConsumption)) throw new TypeError('Fuel consumption should be safe integer');
-        if (fuelConsumption < 0) {
-            throw new RangeError('Fuel consumption can\'t be less than 0');
+    set fuelConsumption(fuelConsumptionVolume) {
+        if (!Number.isFinite(fuelConsumptionVolume)) throw new TypeError('Fuel consumption should be a finite integer');
+        if (fuelConsumptionVolume < 0) {
+            throw new RangeError('Fuel consumption volume can\'t be less than 0');
         };
-        this.#fuelConsumption = fuelConsumption;
+        this.#fuelConsumption = fuelConsumptionVolume;
     };
     get fuelConsumption() {
         return this.#fuelConsumption;
@@ -77,6 +77,9 @@
     get milege() {
         return this.#mileage;
     };
+    get isStarted(){
+        return this.#isStarted;
+    }
 
     start() {
         if (this.isStarted) throw new Error('Engine is already started');
@@ -87,18 +90,16 @@
         this.#isStarted = false;
     };
     fillUpGasTank(volume) {
-        if (!Number.isSafeInteger(volume) || volume <= 0) throw new TypeError('Invalid fuel volume for fillng up gas tank');
+        if (!Number.isFinite(volume) || volume <= 0) throw new TypeError('Invalid fuel volume for fillng up gas tank');
         if (this.#maxFuelVolume < this.#currentFuelVolume + volume) throw new RangeError('Gas tank is overflowed');
         this.#currentFuelVolume += volume;
     };
     drive(speed, hours) {
-        if (!Number.isSafeInteger(speed) || speed <= 0) throw new TypeError('Invalid speed');
-        if (!Number.isSafeInteger(hours) || hours <= 0) throw new TypeError('Invalid hours count');
+        if (!Number.isFinite(speed) || speed <= 0) throw new TypeError('Invalid speed');
+        if (!Number.isFinite(hours) || hours <= 0) throw new TypeError('Invalid hours count');
         if (!this.#isStarted) throw new Error('Engine should be started');
         let distantion = speed * hours;
-        console.log(`distantion`,distantion);
         let driveConsuption = distantion * this.#fuelConsumption / 100;
-        console.log(`driveConsuption`,driveConsuption)
         if (this.#currentFuelVolume - driveConsuption < 0) {
             throw new RangeError('Not enough fuel');
         } else {
@@ -106,27 +107,4 @@
             this.#mileage += distantion;
         };
     };
-
 };
-
-const car = new Car();
-
-car.brand = 'tesla';
-car.model = 's';
-car.fuelConsumption = 5;
-car.maxFuelVolume = 20;
-car.maxSpeed = 200;
-car.yearOfManufacturing = 2021;
-console.log(car.currentFuelVolume);
-car.fillUpGasTank(20);
-console.log(car.currentFuelVolume)
-car.start();
-car.drive(60,3);
-console.log(car.milege)
-console.log(car.currentFuelVolume, `currentFuelVolume`);
-car.drive(80,2);
-console.log(car.milege, 'milage');
-car.shutDownEngine();
-car.start();
-car.fillUpGasTank(20 - car.currentFuelVolume);
-car.drive(200,1);
